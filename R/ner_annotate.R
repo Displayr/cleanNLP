4b4c2fe57bba7ebfa9c8820714d53ad4bb111bd5
jrwishart <justin.r.wishart@gmail.com>
2019-11-01 18:09:59 +1100

    Add ability to multi-thread NER [ci skip]

--- R/ner_annotate.R
+++ R/ner_annotate.R
@@ -1,6 +1,6 @@
-#' Run the annotation pipeline on a set of documents to extract entities
+#' @title Run the annotation pipeline on a set of documents to extract entities
 #'
-#' Runs the entity detection algorithms from CoreNLP using CoreNLP java library via rJava.
+#' @description Runs the entity detection algorithms from CoreNLP using CoreNLP java library via rJava.
 #' It expects the CoreNLP java object to already be initialised with rJava with a call to 
 #' \code{cnlp_init_corenlp_custom} with the appropriate annotators setup for named entity
 #' recognition and a path to an input file that has the input text separately by new lines.
@@ -9,8 +9,6 @@
 #' in the document whre each entity occurs and the entity type. If no entities are detected for a
 #' document then an empty data.frame with no rows is returned.
 #'
-#' @param input.file a character string showing the path to the file to be processed. The file should
-#'    have text with Unix style line endings (will throw Nullpointer exception if not)
 #' @param entity.mentions.only Logical to specify if only entity mention output from CoreNLP is used
 #'    in the extraction. If TRUE, this will extract personal pronouns as well as standard entities.
 #'    The benefit of the entity.mention output is it groups words that are from the same entity. E.g.
@@ -27,7 +25,6 @@
 #'        \item \code{entity} character: The extracted entity word (e.g. William)
 #'        \item \code{entity.tyoe} character: The entity type of the extracted entity (e.g. Person)
 #'    }
-#' @importFrom jsonlite fromJSON
 #' @importFrom rJava .jcall
 #' @examples
 #' \dontrun{
@@ -42,17 +39,40 @@
 #'             "true")
 #' 
 #' cnlp_init_corenlp_custom(language = "en", mem = "2g", keys = keys, values = values)
-#' simple.output <- NERAnnotate(input.file)
+#' simple.output <- NERAnnotate()
 #' }
 #' @export
-NERAnnotate <- function(input.file, entity.mentions.only = FALSE) {
-  
+NERAnnotate <- function(entity.mentions.only = FALSE) {
+    
     if(!volatiles$corenlp$init)
         stop("Java CoreNLP not initialized. Named Entity Recognition cannot be executed.")
-  
+    if(is.null(volatiles$corenlp$properties$file))
+        stop("Java CoreNLP properties doesn't have an input file path.",
+             "Please set the input file path via cnlp_init_corenlp_custom")
     .jcall(volatiles$corenlp$corenlp, "V", "run")
   
-    output <- fromJSON(paste0(input.file, ".json"))
+    parseNEROutput(file = volatiles$corenlp$properties$file, entity.mentions.only = entity.mentions.only)
+}
+#' @title Parse the NER output from CoreNLP
+#' @description Reads the JSON file output from coreNLP ner annotator and coverts to a data frame
+#' @param file Character string with the path of the input file without extension to be processed
+#' @param entity.mentions.only Logical to determine if only the entity mention output is processed. 
+#' The entity mention output includes entities and any grouped words e.g. John Smith is a single entity
+#' instead of two separate entities. However, this output also includes personal pronouns (he, she etc)
+#' as entities. If this logical is set to FALSE, the entity output is validated agains the token output.
+#' The token output specifies if a word is an entity and doesn't classify personal pronouns as entities.
+#' @return data.frame with three columns, 
+#' \itemize{
+#'        \item \code{id} integer: the row index of the input file that has an extracted entity.
+#'        \item \code{entity} character: The extracted entity word (e.g. William)
+#'        \item \code{entity.tyoe} character: The entity type of the extracted entity (e.g. Person)
+#'    }
+#' @importFrom jsonlite fromJSON
+parseNEROutput <- function(file, entity.mentions.only)
+{
+    while (!file.exists(paste0(file, ".json")))
+        Sys.sleep(1)
+    output <- fromJSON(paste0(file, ".json"))
     ner.mentions = output$sentences$entitymentions
     response = sapply(ner.mentions, nrow)
     if(all(sapply(response, is.null)))
@@ -61,23 +81,23 @@ NERAnnotate <- function(input.file, entity.mentions.only = FALSE) {
         if(!entity.mentions.only)
         {
             # Validate ner.mentions against the token output
-            ner.mentions <- mapply(function(x, y) 
+            ner.mentions <- mapply(function(x, y)
+            {
+                if(nrow(y) != 0)
                 {
-                    if(nrow(y) != 0)
+                    idx <- sapply(1:nrow(y), function(i)
                     {
-                        idx <- sapply(1:nrow(y), function(i)
-                            {
-                                z <- y[i, ]
-                                ind <- c(z$tokenBegin + 1, z$tokenEnd)
-                                all(x[ind, ]$ner != "O")
-                            })
+                        z <- y[i, ]
+                        ind <- c(z$tokenBegin + 1, z$tokenEnd)
+                        all(x[ind, ]$ner != "O")
+                    })
                     if(any(idx))
                         y <- y[idx, ]
                     else
                         return(data.frame())
-                    }
+                }
                 y
-                }, x = output$sentences$tokens, y = ner.mentions)
+            }, x = output$sentences$tokens, y = ner.mentions, SIMPLIFY = FALSE)
             # Check if filtered ner is not empty
             if(all(sapply(ner.mentions, nrow) == 0))
                 return(data.frame(id = character(), entity = character(), entity.type = character()))
@@ -94,4 +114,101 @@ NERAnnotate <- function(input.file, entity.mentions.only = FALSE) {
         names(out) <- c("id", "entity", "entity.type")
     }
     out
+}
+
+#' @title Run the annotation pipeline on a set of documents to extract entities with multiple threads
+#'
+#' @description Runs the entity detection algorithms from CoreNLP using CoreNLP java library via rJava.
+#' It expects the CoreNLP java object to already be initialised with rJava with a call to 
+#' \code{cnlp_init_corenlp_custom} with the appropriate annotators setup for named entity
+#' recognition and a path to a filelist that contains a list of files (one file path per line)
+#' each of the pointed file paths has the input text separately by new lines.
+#' The input file must have Unix style line endings or will cause the CoreNLP java call to crash
+#' with a null pointer exception. The function returns a \code{data.frame} showing the location
+#' in the document whre each entity occurs and the entity type. If no entities are detected for a
+#' document then an empty data.frame with no rows is returned.
+#' 
+#' @param separate.text Logical to specify if the threaded calculation was used on a single corpus 
+#' that was split into separate processing threads. If \code{TRUE}, the files in the filelist are 
+#' assumed to be unique (not related) and each output is treated separately. If \code{FALSE} the 
+#' input files are assumed to be split from the same text input data. Then document ids are 
+#' cumulative over all the threads and need to be adjusted and combined into a single data.frame.
+#' @param row.adjustment Integer vector of cumulative counts of lines in each of the previous files.
+#'    If the file split is already known, this can be used to speed up computation (prevent \code{readLines}
+#'    executing on of a big bunch of files only to compute number of lines).
+#' @param entity.mentions.only Logical to specify if only entity mention output from CoreNLP is used
+#'    in the extraction. If TRUE, this will extract personal pronouns as well as standard entities.
+#'    The benefit of the entity.mention output is it groups words that are from the same entity. E.g.
+#'    'John Smith' is a single person entity and 'New York City' is a single location entity. The 
+#'    entity mention output also classifies personal pronouns as entities and will be extracted.
+#'    If FALSE, the entity.mentions output from CoreNLP is validated against the CoreNLP token output.
+#'    The token output also identifies entities on the single word level and it doesn't classify personal
+#'    pronouns as entities. The net effect if set to FALSE is that entity mentions are extracted but
+#'    personal pronouns and other potential entity mentions that are not entities on the token level
+#'    are not extracted.
+#' @return If \code{separate.text} is \code{FALSE}, then a single data.frame with the details of the 
+#' detected entities is returned. If \code{TRUE}, a list of data.frames is returned where each list 
+#' element refers to each file input. The output data.frame has three
+#'    columns. \itemize{
+#'        \item \code{id} integer: the row index of the input file that has an extracted entity.
+#'        \item \code{entity} character: The extracted entity word (e.g. William)
+#'        \item \code{entity.tyoe} character: The entity type of the extracted entity (e.g. Person)
+#'    }
+#' @importFrom jsonlite fromJSON
+#' @importFrom rJava .jcall
+#' @examples
+#' \dontrun{
+#' simple.input.test <- c("John is a person", "Google is a company", "This is nothing")
+#' 
+#' input.file.1 <- tempfile()
+#' file <- file(input.file.1, "wb") # need linux style line endings
+#' writeLines(simple.input.test[1:2], con = file)
+#' close(file)
+#' input.file.2 <- tempfile()
+#' file <- file(input.file.2, "wb") # need linux style line endings
+#' writeLines(simple.input.test[3:4], con = file)
+#' close(file)
+#' filelist <- tempfile()
+#' file <- file(filelist, "wb") # need linux style line endings
+#' writeLines(c(input.file.1, input.file.2), con = file)
+#' close(file)
+#' 
+#' keys <- c("ssplit.eolonly", "annotators", "outputFormat", "fileList", "outputDirectory",
+#'           "output.prettyPrint")
+#' values <- c("true", "tokenize,ssplit,pos,lemma,ner", "json", filelist, dirname(filelist),
+#'             "true")
+#' 
+#' cnlp_init_corenlp_custom(language = "en", mem = "2g", keys = keys, values = values)
+#' simple.output <- MultiThreadedNERAnnotate()
+#' }
+#' @export
+MultiThreadedNERAnnotate <- function(separate.text = FALSE, row.adjustment = NULL,
+                                     entity.mentions.only = FALSE)
+{
+    if(!volatiles$corenlp$init)
+        stop("Java CoreNLP not initialized. Named Entity Recognition cannot be executed.")
+    if(is.null(volatiles$corenlp$properties$fileList))
+        stop("The CoreNLP java properties doesn't have the fileList property set. ",
+             "Multi-threaded mode can only be used if fileList property is defined in the CoreNLP Java properties")
+    .jcall(volatiles$corenlp$corenlp, "V", "run")
+    
+    file.paths <- readLines(volatiles$corenlp$properties$fileList)
+    output.file.paths <- paste0(file.paths, ".json")
+    if(!separate.text)
+    {
+        if(is.null(row.adjustment))
+            row.adjustment <- c(0, sapply(file.paths[-length(file.paths)], function(x) length(readLines(x)), USE.NAMES = FALSE))
+    }
+    else
+        row.adjustment <- rep(0, length(file.paths))
+    output <- mapply(function(file, count, mention) {
+        out <- parseNEROutput(file, entity.mentions.only = mention)
+        if (nrow(out) > 0 & count != 0)
+            out$id = out$id + count
+        out
+    }, file = file.paths, count = row.adjustment, mention = entity.mentions.only,
+    SIMPLIFY = FALSE)
+    if(!separate.text)
+        output <- do.call(rbind.data.frame, unname(output))
+    output
 }
--- tests/testthat/test-entity.R
+++ tests/testthat/test-entity.R
@@ -9,7 +9,9 @@ simple.input.test <- c("There is a person called Julie that went down the lane. 
 
 # Test result when no entities are present
 none.input <- c("There is no entity here",
-                "Nor here")
+                "Nor here", 
+                "This is another null entity sentence",
+                "Nothing to extract here")
 
 simple.expected <- structure(list(id = c(1L, 3L, 4L, 4L, 4L), 
                                   entity = c("Julie", "Hong Kong", "two", "Jane", "John"),
@@ -17,57 +19,188 @@ simple.expected <- structure(list(id = c(1L, 3L, 4L, 4L, 4L),
                              class = "data.frame",
                              row.names = c(NA, -5L))
 
+multiple.expected <- list(structure(list(id = 1L, entity = "Julie", entity.type = "PERSON"),
+                                        class = "data.frame", row.names = c(NA,-1L)),
+                              structure(list(id = c(1L, 2L, 2L, 2L), 
+                                             entity = c("Hong Kong", "two", "Jane", "John"), 
+                                             entity.type = c("CITY", "NUMBER", "PERSON", "PERSON")),
+                                        class = "data.frame", row.names = c(NA, -4L)))
+
 simple.with.pronouns.expected <- structure(list(id = c(1L, 1L, 3L, 4L, 4L, 4L), 
-                                  entity = c("Julie", "She", "Hong Kong", "two", "Jane", "John"),
-                                  entity.type = c("PERSON", "PERSON", "CITY", "NUMBER", "PERSON",
-                                                  "PERSON")),
-                             class = "data.frame",
-                             row.names = c(NA, -6L))
+                                                entity = c("Julie", "She", "Hong Kong", "two", "Jane", "John"),
+                                                entity.type = c("PERSON", "PERSON", "CITY", "NUMBER", "PERSON",
+                                                                "PERSON")),
+                                           class = "data.frame",
+                                           row.names = c(NA, -6L))
 
 pronouns <- c("he's", "hes", "he is", "He is", "He Is", "she's", "She is")
 
 none.expected <- data.frame(id = character(), entity = character(), entity.type = character())
+
+multi.none.expected <- list(structure(list(id = structure(integer(0), .Label = character(0), class = "factor"), 
+                                           entity = structure(integer(0), .Label = character(0), class = "factor"), 
+                                           entity.type = structure(integer(0), .Label = character(0), class = "factor")),
+                                      class = "data.frame", row.names = integer(0)), 
+                            structure(list(id = structure(integer(0), .Label = character(0), class = "factor"), 
+                                           entity = structure(integer(0), .Label = character(0), class = "factor"), 
+                                           entity.type = structure(integer(0), .Label = character(0), class = "factor")),
+                                      class = "data.frame", row.names = integer(0)))
+
+keys <- c("ssplit.eolonly", "annotators", "outputFormat")
+values <- c("true", "tokenize,ssplit,pos,lemma,ner", "json")
+
+all.single.entity <- as.character(1:3)
+
+all.single.output <- structure(list(id = 1:3,
+                                    entity = c("1", "2", "3"),
+                                    entity.type = c("NUMBER", "NUMBER", "NUMBER")),
+                               class = "data.frame", row.names = c(NA, -3L))
 
 # If this is throwing errors that you need to download Core NLP then the way to get testthat to 
 # find CORENLP is to set CORENLP as a system environment variable with the path to CoreNLP
 # CoreNLP directories in the package installation cannot be located by testthat
 # Nor found by R CMD check 
 # E.g. cleanNLP::cnlp_download_corenlp()
-test_that("NERAnnotate consistency", {
-  
-  tmp.file <- tempfile()
-  
-  file <- file(tmp.file, "wb")
-  writeLines(simple.input.test, con = file)
-  close(file)
-  
-  keys <- c("ssplit.eolonly", "annotators", "outputFormat", "file", "outputDirectory")
-  values <- c("true", "tokenize,ssplit,pos,lemma,ner", "json", tmp.file, dirname(tmp.file))
-  
-  # Expect error if NERAnnotate is called before corenlp is initialised.
-  expect_error(NERAnnotate(tmp.file),
-               "^Java CoreNLP not initialized. Named Entity Recognition cannot be executed.$")
-  
-  cnlp_init_corenlp_custom(language = "en", mem = "2g", keys = keys, values = values, 
-                           corenlp.only = TRUE)
-  
-  expect_error(simple.output <- NERAnnotate(tmp.file), NA)
-  expect_identical(simple.output, simple.expected)
-  
-  expect_error(simple.output.with.pronouns <- NERAnnotate(tmp.file, entity.mentions.only = TRUE), NA)
-  expect_identical(simple.output.with.pronouns, simple.with.pronouns.expected)
-  
-  file <- file(tmp.file, "wb")
-  writeLines(none.input, con = file)
-  close(file)
-  
-  none.output <- NERAnnotate(tmp.file)
-  expect_identical(none.output, none.expected)
-  
-  file <- file(tmp.file, "wb")
-  writeLines(pronouns, con = file)
-  close(file)
-  
-  expect_error(pronoun.output.after.validation <- NERAnnotate(tmp.file, entity.mentions.only = FALSE), NA)
-  expect_identical(pronoun.output.after.validation, none.expected)
+
+test_that("NERAnnotate consistency and MultiThreadNERAnnotate setup", {
+
+    tmp.file <- tempfile()
+
+    file <- file(tmp.file, "wb")
+    writeLines(simple.input.test, con = file)
+    close(file)
+
+    basic.keys <- c(keys, "file", "outputDirectory")
+    basic.values <- c(values, tmp.file, dirname(tmp.file))
+
+    # Expect error if NERAnnotate is called before corenlp is initialised.
+    expect_error(NERAnnotate(),
+                 "^Java CoreNLP not initialized. Named Entity Recognition cannot be executed.$")
+    expect_error(MultiThreadedNERAnnotate(),
+                 "^Java CoreNLP not initialized. Named Entity Recognition cannot be executed.$")
+
+    r.session <- callr::r_session$new()
+
+    # Start a new R session, initialize core NLP
+    r.session$run(function(keys, values)
+        cleanNLP::cnlp_init_corenlp_custom(language = "en", mem = "2g", keys = keys, values = values,
+                                           corenlp.only = TRUE),
+        args = list(keys = basic.keys, values = basic.values))
+
+    expect_error(r.session$run(function() cleanNLP::MultiThreadedNERAnnotate("test")),
+                 regex = "Multi-threaded mode can only be used if fileList property is defined in the CoreNLP Java properties$",
+                 class = "callr_status_error")
+
+    expect_error(simple.output <- r.session$run(function()
+        cleanNLP::NERAnnotate()),
+        NA)
+    expect_identical(simple.output, simple.expected)
+
+    expect_error(simple.output.with.pronouns <- r.session$run(function()
+        cleanNLP::NERAnnotate(entity.mentions.only = TRUE)),
+        NA)
+    expect_identical(simple.output.with.pronouns, simple.with.pronouns.expected)
+
+    file <- file(tmp.file, "wb")
+    writeLines(none.input, con = file)
+    close(file)
+
+    expect_error(none.output <- r.session$run(function(tmp.file)
+        cleanNLP::NERAnnotate(tmp.file),
+        args = list(tmp.file = tmp.file)),
+        NA)
+    expect_identical(none.output, none.expected)
+
+    file <- file(tmp.file, "wb")
+    writeLines(pronouns, con = file)
+    close(file)
+
+    expect_error(pronoun.output.after.validation <- r.session$run(function()
+        cleanNLP::NERAnnotate(entity.mentions.only = FALSE)),
+        NA)
+    expect_identical(pronoun.output.after.validation, none.expected)
+    
+    file <- file(tmp.file, "wb")
+    writeLines(all.single.entity, con = file)
+    close(file)
+    
+    expect_error(all.single.entity.output <- r.session$run(function()
+      cleanNLP::NERAnnotate(entity.mentions.only = FALSE)), NA)
+    expect_identical(all.single.entity.output, all.single.output)
+    
+    r.session$close()
 })
+
+# Split text input vector x into number of pieces specified by threads
+# Write temp file for each piece. The create a single file 
+# that contains the path for each of the generated files (one per line)
+splitInput <- function(x, threads, filelist.path)
+{
+    split.groups <- ceiling(seq_along(x)/length(x) * threads)
+    split.x <- split(x, split.groups)
+    file.paths <- NULL
+    for (i in 1:threads)
+    {
+        tmp.file <- tempfile()
+        file <- file(tmp.file, "wb")
+        writeLines(split.x[[i]], con = file)
+        close(file)
+        file.paths <- append(file.paths, tmp.file)
+    }
+    file <- file(filelist.path, "wb")
+    writeLines(file.paths, con = file)
+    close(file)
+    return(invisible())
+}
+
+test_that("MultiThreadNERAnnotate consistency", {
+    
+    tmp.file <- tempfile()
+    threads <- 2
+    
+    splitInput(simple.input.test, threads = threads, filelist.path = tmp.file)
+    
+    multi.keys <- c(keys, "fileList", "outputDirectory", "threads")
+    multi.values <- c(values, tmp.file, dirname(tmp.file), threads)
+    
+    r.session <- callr::r_session$new()
+    
+    # Start a new R session, initialize core NLP
+    r.session$run(function(keys, values)
+        cleanNLP::cnlp_init_corenlp_custom(language = "en", mem = "2g", keys = keys, values = values,
+                                           corenlp.only = TRUE),
+        args = list(keys = multi.keys, values = multi.values))
+    
+    expect_error(multiple.output <- r.session$run(function(separate.text)
+        cleanNLP::MultiThreadedNERAnnotate(separate.text = separate.text),
+        args = list(separate.text = TRUE)), NA)
+    expect_equal(unname(multiple.output), multiple.expected)
+    
+    expect_error(single.output <- r.session$run(function(separate.text)
+        cleanNLP::MultiThreadedNERAnnotate(separate.text = separate.text),
+        args = list(separate.text = FALSE)), NA)
+    expect_equal(single.output, simple.expected)
+    
+    row.adj <- c(0, 2)
+    
+    expect_error(single.output.with.row <- r.session$run(function(separate.text, row.adjustment)
+        cleanNLP::MultiThreadedNERAnnotate(separate.text = separate.text, row.adjustment = row.adjustment),
+        args = list(separate.text = FALSE, row.adjustment = row.adj)), NA)
+    
+    expect_equal(single.output.with.row, simple.expected)
+    
+    splitInput(none.input, threads = 2, filelist.path = tmp.file)
+    
+    expect_error(multiple.none.output <- r.session$run(function(separate.text)
+        cleanNLP::MultiThreadedNERAnnotate(separate.text = separate.text),
+        args = list(separate.text = TRUE)), NA)
+    expect_equal(unname(multiple.none.output), multi.none.expected)
+    
+    expect_error(single.none.output <- r.session$run(function(separate.text)
+        cleanNLP::MultiThreadedNERAnnotate(separate.text = separate.text),
+        args = list(separate.text = FALSE)), NA)
+    expect_equal(single.none.output, none.expected)
+    
+    r.session$close()
+})
+
